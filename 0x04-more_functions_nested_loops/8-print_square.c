67c0a15a226abd5eefc6a92d65744b3be4f02786
Shopye <sholapeajayi@gmail.com>
2022-12-17 11:40:58 -0800

    Print squared

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,24 +3,20 @@
 /**
  * print_square - prints a square
  * @size: the size of the square
- * Return: 0
+ * Return: void
  */
 void print_square(int size)
 {
-	int a;
-	int b;
+	int i, j;
 
 	if (size <= 0)
 		_putchar('\n');
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (a = 0; a < size; a++)
+		for (j = 0; j < (size); j++)
 		{
-			for (b = 0; b < size; b++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 }
