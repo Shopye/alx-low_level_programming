d3345624b35203e78db18279c7e331083816fb50
Shopye <sholapeajayi@gmail.com>
2022-12-22 09:42:17 -0800

    task 0 corrected

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-       	int i;
-	int j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		 i++;
-	}
-	dest[i] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
